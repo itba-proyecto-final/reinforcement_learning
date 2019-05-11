53a71b379e00110daead11803cefb9414d71590e
natalia navas <natinavas@gmail.com>
2019-05-11 18:57:00 -0300

    Fix in imports

--- enviroment/g_training_from_experiment.py
+++ enviroment/g_training_from_experiment.py
@@ -2,7 +2,7 @@ import gym
 import numpy as np
 import gym_chase
 
-from enviroment.q_learning import write_q_table_file
+from enviroment.q_tools import write_q_table_file
 
 env = gym.make('chase-mental-v0')
 LEARNING_RATE = .2
