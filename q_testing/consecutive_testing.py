9ceeb408548e6fc4bb33966d7000e12f020c76e8
natalia navas <natinavas@gmail.com>
2019-07-13 18:41:04 -0300

    Add error bars to avg step plot

--- q_testing/consecutive_testing.py
+++ q_testing/consecutive_testing.py
@@ -20,6 +20,8 @@ if __name__ == "__main__":
     steps = []  # Amount of steps it takes to reach the goal in each testing iteration
     average_steps = []  # Amount of testing steps for each experiment
     q_table = None
+    lower_error = []
+    upper_error = []
 
     # Test with an empty Q Table so that the game plays randomly to compare amount of steps
     test_env = gym.make('chase-v0')
@@ -29,6 +31,8 @@ if __name__ == "__main__":
     all_steps, avg_steps = test_q_table(env=test_env, q_table=q_table)
     steps.extend(all_steps)
     average_steps.append(avg_steps)
+    lower_error.append(avg_steps - min(all_steps))
+    upper_error.append(max(all_steps) - avg_steps)
 
     for experiment in experiments:
 
@@ -44,6 +48,8 @@ if __name__ == "__main__":
         all_steps, avg_steps = test_q_table(env=test_env, q_table=q_table)
         steps.extend(all_steps)
         average_steps.append(avg_steps)
+        lower_error.append(avg_steps - min(all_steps))
+        upper_error.append(max(all_steps) - avg_steps)
 
     for step in steps:
         print(step)
@@ -53,8 +59,11 @@ if __name__ == "__main__":
     for avg_steps in average_steps:
         print(avg_steps)
 
-    plt.scatter(x=range(len(average_steps)), y=average_steps)
+    x = range(len(average_steps))
+    y = average_steps
+    plt.scatter(x=x, y=y)
     plt.xticks(np.arange(0, len(average_steps), 1))
+    plt.errorbar(x, y, yerr=[lower_error, upper_error], fmt='o')
     plt.title("Average steps per experiment", fontsize=20)
     plt.xlabel("Experiments", fontsize=10)
     plt.ylabel("Average steps", fontsize=10)
